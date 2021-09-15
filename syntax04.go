1e70d4860c7b3c1c529903ad4dabf00c48fa9e4d
kowonyeong <ganenode@naver.com>
2021-09-16 03:01:39 +0900

    version03

--- syntax04.go
+++ syntax04.go
@@ -6,24 +6,24 @@ import (
 	"time"
 )
 
-//난수 추출된 수의 소수 판정 프로그램 v0.2
+//난수 추출된 수의 소수 판정 프로그램 v0.3
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
-	check := false
+	check := true
 
 	number := rand.Intn(150) + 2 // 0과 1제외, 2~151 사이의 수
 	//fmt.Println(number)
 	//number = 47
 	for i := 2; i < number; i++ {
 		if (number % i) == 0 {
-			check = true
+			check = false
 		}
 	}
-	if check == false {
-		fmt.Print(number, "는(은) 솟수입니다!")
+	if check == true {
+		fmt.Print(number, "는(은) 소수입니다!")
 	} else {
-		fmt.Print(number, "는(은) 솟수가 아닙니다!")
+		fmt.Print(number, "는(은) 소수가 아닙니다!")
 	}
 }
